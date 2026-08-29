2fb87496814043708c879d538fbdd4e33211b6c1
suacayipkisi <konuki8523@gmail.com>
2026-08-29 22:08:17 +0300

    GPLv3 licensing ANFINEN

--- src/main.cpp
+++ src/main.cpp
@@ -38,8 +38,7 @@
 
 #ifdef _WIN32
 extern "C" {
-    __deCopyright (c) 2026 Ufuk Deniz Konuk
-clspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
     __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
 #endif
